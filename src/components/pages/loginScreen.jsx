49a1a4d65de95d0b88044e59c799f5c5f6efc4c6
Bagoesardhia <96311137+Bagoesardhia@users.noreply.github.com>
2022-11-27 02:17:50 +0700

    Create loginScreen.jsx

---
+++ src/components/pages/loginScreen.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function loginScreen () {
+    return(
+        <div className='loginScreen'>
+            <h1> INI LOGIN SCREEN</h1>
+        </div>
+    )
+}
